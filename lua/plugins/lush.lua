42db877fbeb70ab5eba3583d246134f628d38e1e
Thiago Teixeira <thiago.teixeira@snowflake.com>
2024-12-08 23:57:29 -0800

    Auto-switch between Mac and Linux paths for Lush theme

--- lua/plugins/lush.lua
+++ lua/plugins/lush.lua
@@ -1,4 +1,13 @@
+local linux_dir = "/homex/tvst/Projects/config/repos/neophile"
+local mac_dir = "/Users/tteixeira/Projects/config/repos/neophile"
+
+local dir_name = linux_dir
+
+if vim.fn.isdirectory(dir_name) == 0 then
+  dir_name = mac_dir
+end
+
 return {
   "rktjmp/lush.nvim",
-  { dir = "/home/tvst/Projects/config/repos/neophile", lazy = true },
+  { dir = dir_name, lazy = true },
 }
