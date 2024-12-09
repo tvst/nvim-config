42a4abe0b4c14817b5a15d76cfb46a1ad00a0358
Thiago Teixeira <thiago@streamlit.io>
2024-12-08 16:33:40 -0800

    Add bracketed

---
+++ lua/plugins/bracketed.lua
@@ -0,0 +1,5 @@
+return {
+  "echasnovski/mini.bracketed",
+  version = "*",
+  config = true,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,40 +4,10 @@ return { -- Highlight, edit, and navigate code
   main = "nvim-treesitter.configs", -- Sets main module to use for opts
 
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    enabled = false,
-    ensure_installed = {
-      "bash",
-      "c",
-      "diff",
-      "fish",
-      "gitcommit",
-      "html",
-      "javascript",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "typescript",
-      "vim",
-      "vimdoc",
-    },
-
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-
-    indent = { enable = true, disable = { "ruby" } },
-  },
+  opts = function(_, opts)
+    opts.auto_install = true
+    opts.textobjects = { enable = false }
+  end,
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
