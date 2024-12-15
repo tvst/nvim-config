c69e3d002f5a7c91a1060989344483580d2a99a5
Thiago Teixeira <thiago.teixeira@snowflake.com>
2024-12-14 17:00:04 -0800

    Update to latest lazyvim and remove silly animations and such

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
-
 vim.keymap.set("n", "<S-h>", "gT", { desc = "Prev Tab" })
 vim.keymap.set("n", "<S-l>", "gt", { desc = "Next Tab" })
 
