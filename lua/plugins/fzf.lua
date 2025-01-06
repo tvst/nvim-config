d15534f0dd6cf45eecbda8b7b836182632710f23
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-01-06 15:30:28 -0800

    Make fzf space-space use cwd

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,4 +3,7 @@ return {
   opts = function(_, opts)
     opts.winopts.backdrop = 100
   end,
+  keys = {
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+  },
 }
