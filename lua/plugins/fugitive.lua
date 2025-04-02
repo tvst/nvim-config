516d07cb1e86b95da99a30b350121c539f3a7d5c
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-04-01 22:48:10 -0700

    Make fugitive the only Git solution

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,39 +4,45 @@ return {
     "tpope/vim-fugitive",
     keys = {
       {
-        "<leader>ggs",
-        "<cmd>:Git<cr>",
+        "<leader>gs",
+        "<cmd>:tab :Git<cr>",
         desc = "Git status",
+        remap = true,
       },
 
       {
-        "<leader>ggl",
+        "<leader>gl",
         "<cmd>:Gclog!<cr>",
         desc = "Git repo log",
+        remap = true,
       },
 
       {
         "<localleader>gl",
         "<cmd>:0Gclog!<cr>",
         desc = "Git file log",
+        remap = true,
       },
 
       {
         "<localleader>gb",
         "<cmd>:Git blame<cr>",
         desc = "Git blame",
+        remap = true,
       },
 
       {
         "<localleader>gd",
         "<cmd>:Gvdiffsplit<cr>",
         desc = "Git file diff",
+        remap = true,
       },
 
       {
         "<localleader>gr",
         "<cmd>:Gread<cr>",
         desc = "Git reset file",
+        remap = true,
       },
     },
   },
