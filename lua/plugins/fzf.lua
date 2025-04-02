3144fd069f58852e0f1d3d747b401ecb066b77f6
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-04-01 21:53:09 -0700

    Remove fzf

--- lua/plugins/fzf.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  opts = function(_, opts)
-    opts.winopts.backdrop = 100
-  end,
-  keys = {
-    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-  },
-}
