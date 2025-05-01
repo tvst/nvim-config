4a59ec5dddb6b741623e86cc3f3acd31aac17d23
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-04-30 17:03:44 -0700

    Move some default options down in which-key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,4 @@
 local add = require("which-key").add
-add({ "<leader>-", "<C-W>s", desc = "Split below" })
-add({ "<leader>|", "<C-W>v", desc = "Split right" })
-add({ "<leader>.", function() require("snacks").picker.resume() end, desc = "Resume last picker" })
 
 require "keymaps-file"
 require "keymaps-editor"
@@ -18,3 +15,7 @@ require "keymaps-goto"
 require "keymaps-nav"
 require "keymaps-local"
 require "keymaps-fixes"
+
+add({ "<leader>-", "<C-W>s", desc = "Split below" })
+add({ "<leader>|", "<C-W>v", desc = "Split right" })
+add({ "<leader>.", function() require("snacks").picker.resume() end, desc = "Resume last picker" })
