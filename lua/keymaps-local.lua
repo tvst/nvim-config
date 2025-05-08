6b4c53f356b58fab46e0ded6c25cccb3a6ee9c44
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-05-07 23:07:51 -0700

    no-op cleanups

--- lua/keymaps-local.lua
+++ lua/keymaps-local.lua
@@ -5,9 +5,9 @@ add({ "<localleader><localleader>", "<cmd>e #<cr>", desc = "Switch to prev buffe
 
 -- Clear search, diff update and redraw
 add({
-  "<localleader>r",
-  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
-  { desc = "[R]edraw" }
+    "<localleader>r",
+    "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+    { desc = "[R]edraw" }
 })
 
 -- Mappings for diffs
@@ -15,6 +15,7 @@ add({ "<localleader>d", "<cmd>diffthis<cr>", desc = "[D]iff file" })
 add({ "<localleader>du", "<cmd>diffupdate<cr>", desc = "Diff [U]pdate" })
 add({ "<localleader>dd", "<cmd>diffoff<cr>", desc = "Stop [D]iff" })
 
--- add({ "<localleader>q", "<cmd>q<cr>", noremap = true, silent = true, desc = "Close window" })
 add({ "<localleader>q", "<C-W>q", desc = "[C]lose window" })
 require("snacks").toggle.zoom():map("<localleader>z")
+
+-- Local Git mappings at: keymaps-git.lua
