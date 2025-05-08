57324eb5b592c64d0114b73f27507d3fc650aaf6
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-05-08 16:36:42 -0700

    Swap tab-tab and tab-e (now tab-n)

--- lua/keymaps-tab.lua
+++ lua/keymaps-tab.lua
@@ -1,5 +1,5 @@
 local add = require("which-key").add
 
-add({ "<leader><tab><tab>", "<cmd>tabnew<cr>", desc = "New file in new tab" })
-add({ "<leader><tab>e", "<cmd>tabedit %<cr>", desc = "Open in new tab" })
+add({ "<leader><tab><tab>", "<cmd>tabedit %<cr>", desc = "Open in new tab" })
+add({ "<leader><tab>n", "<cmd>tabnew<cr>", desc = "Open new file in a tab" })
 add({ "<leader><tab>q", "<cmd>tabclose<cr>", desc = "Close tab" })
