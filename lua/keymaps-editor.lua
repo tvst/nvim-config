d05a7c17dc1811d8b2ca85f7c7eab5c69a917fd8
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-06-12 17:21:42 -0700

    Make autoformat toggle work

--- lua/keymaps-editor.lua
+++ lua/keymaps-editor.lua
@@ -6,49 +6,28 @@ Snacks.toggle.inlay_hints():map("<leader>eh")
 Snacks.toggle.line_number():map("<leader>en")
 --Snacks.toggle.option({ "cursorline" }):map("<leader>ec")
 
-local M = {}
+Snacks.toggle
+    .new({
+      id = "Auto Format (buffer)",
+      name = "Auto Format (buffer)",
+      get = function()
+        return not vim.b.disable_autoformat
+      end,
+      set = function(state)
+        vim.b.disable_autoformat = not state
+      end,
+    })
+    :map("<leader>eF")
 
--- From https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/format.lua
-function M.toggle_autoformat(buf)
-  return Snacks.toggle({
-    name = "Auto Format (" .. (buf and "Buffer" or "Global") .. ")",
-    get = function()
-      if not buf then
-        return vim.g.autoformat == nil or vim.g.autoformat
-      end
-      return M.autoformat_enabled()
-    end,
-    set = function(state)
-      M.enable_autoformat(state, buf)
-    end,
-  })
-end
-
-function M.autoformat_enabled(buf)
-  buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
-  local gaf = vim.g.autoformat
-  local baf = vim.b[buf].autoformat
-
-  -- If the buffer has a local value, use that
-  if baf ~= nil then
-    return baf
-  end
-
-  -- Otherwise use the global value if set, or true by default
-  return gaf == nil or gaf
-end
-
-function M.enable_autoformat(enable, buf)
-  if enable == nil then
-    enable = true
-  end
-  if buf then
-    vim.b.autoformat = enable
-  else
-    vim.g.autoformat = enable
-    vim.b.autoformat = nil
-  end
-end
-
-M.toggle_autoformat(true):map("<leader>eF")
-M.toggle_autoformat():map("<leader>ef")
+Snacks.toggle
+    .new({
+      id = "Auto Format (global)",
+      name = "Auto Format (global)",
+      get = function()
+        return not vim.g.disable_autoformat
+      end,
+      set = function(state)
+        vim.g.disable_autoformat = not state
+      end,
+    })
+    :map("<leader>ef")
